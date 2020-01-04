d66cced03dd91f18fe2a6433c3e7518aba3cf24b
Marie Slavina <morefly@yandex.ru>
2020-01-04 17:37:58 +0300

    add auth

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 
 const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 mongoose.connect('mongodb://localhost:27017/newsdb', {
   useNewUrlParser: true,
@@ -16,6 +18,17 @@ mongoose.connect('mongodb://localhost:27017/newsdb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/signin', login);
+app.post('/signup', createUser);
+
+app.use('/', auth, require('./routes/users'));
+app.use('/', auth, require('./routes/articles'));
+
+app.use('*', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.status(404).send({ message: 'The requested resource is not found' });
+});
 
 
 app.listen(PORT);
