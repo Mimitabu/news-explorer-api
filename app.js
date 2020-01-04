aee7f0d1746e4069d2763644ea5a61aafe0f19b6
Marie Slavina <morefly@yandex.ru>
2020-01-04 19:05:42 +0300

    add validation & logger

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+const mongoose = require('mongoose');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
-const mongoose = require('mongoose');
+
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 
@@ -19,16 +23,52 @@ mongoose.connect('mongodb://localhost:27017/newsdb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+// логгер запросов
+app.use(requestLogger);
 
+// аутентификация и авторизация
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+
+// роуты, защищенные авторизацией
 app.use('/', auth, require('./routes/users'));
 app.use('/', auth, require('./routes/articles'));
 
+// роут по умолчанию
 app.use('*', (req, res) => {
   res.set('Content-Type', 'application/json');
   res.status(404).send({ message: 'The requested resource is not found' });
 });
 
+// логгер ошибок
+app.use(errorLogger);
+
+// обработчик ошибок celebrate
+app.use(errors());
+
+// централизованный обработчик ошибок
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
 
 app.listen(PORT);
--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,5 @@
 const routerArticles = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getArticles,
@@ -7,7 +8,17 @@ const {
 } = require('../controllers/articles');
 
 routerArticles.get('/articles', getArticles);
-routerArticles.post('/articles', createArticle); // здесь нужна валидация переданных данных
+routerArticles.post('/articles', celebrate({
+  body: Joi.object().keys({
+    keyword: Joi.string().required().min(2),
+    title: Joi.string().required().min(2),
+    text: Joi.string().required().min(2),
+    date: Joi.string().required(),
+    source: Joi.string().required(),
+    link: Joi.string().required(),
+    image: Joi.string().required(),
+  }),
+}), createArticle); // здесь нужна валидация переданных данных
 routerArticles.delete('/articles', deleteArticle);
 
 module.exports = routerArticles;
