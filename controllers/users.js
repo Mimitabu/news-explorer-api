14a72cf203955f71b2c518468f6d4484b007bc84
Marie Slavina <morefly@yandex.ru>
2020-01-04 16:43:45 +0300

    add users controller

---
+++ controllers/users.js
@@ -0,0 +1,70 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const UnauthorizedError = require('../errors/unauthorized-error');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+// возвращает информацию о пользователе (email и имя)
+const getUser = (req, res, next) => {
+  User.find({})
+    .then((user) => res.send({
+      email: user.email,
+      name: user.name,
+    }))
+    .catch(next);
+};
+// создаёт пользователя с переданными в теле email, password и name
+const createUser = (req, res, next) => {
+  const {
+    name, email, password,
+  } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, email, password: hash,
+    }))
+    .then((user) => res.status(201).send({
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    }))
+    .catch(next);
+};
+
+// проверяет переданные в теле почту и пароль и возвращает JWT
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Invalid email or password');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError('Invalid email or password');
+          }
+          const token = jwt.sign(
+            { _id: user._id },
+            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+            { expiresIn: '7d' },
+          );
+          return res.cookie('jwt', token, {
+            maxAge: 604800000,
+            httpOnly: true,
+            sameSite: true,
+          })
+            .send({ message: 'Successful authorization' })
+            .end();
+        });
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getUser,
+  createUser,
+  login,
+};
