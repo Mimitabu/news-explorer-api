8e3efd4e720dcba0608cebe14c7636f48a36b456
Marie Slavina <morefly@yandex.ru>
2020-01-20 16:59:52 +0300

    fix default router

--- routes/default.js
+++ routes/default.js
@@ -2,9 +2,9 @@ const routerDefault = require('express').Router();
 const NotFoundError = require('../errors/not-found-error');
 const { notFoundErrorMessage } = require('../messagesData');
 
-routerDefault.use('*', (req, res) => {
+routerDefault.use('*', (req, res, next) => {
   res.set('Content-Type', 'application/json');
-  throw new NotFoundError(notFoundErrorMessage);
+  next(new NotFoundError(notFoundErrorMessage));
 });
 
 module.exports = routerDefault;
