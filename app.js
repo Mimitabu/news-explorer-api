fd9ac76afbdbb6ef4aa0781299358d8360d6add1
Marie Slavina <morefly@yandex.ru>
2020-02-20 16:43:55 +0300

    add CORS

--- app.js
+++ app.js
@@ -17,16 +17,18 @@ const mongodb = require('./mongodb');
 
 const { NODE_ENV, MONGO_DB } = process.env;
 
-const whitelist = ['http://localhost:8080', 'http://news-explorer.pw'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-};
+// const whitelist = ['http://localhost:8080', 'http://news-explorer.pw', 'http://api.news-explorer.pw', 'http://localhost:3000'];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// };
+
+
 
 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_DB : mongodb, {
@@ -35,7 +37,16 @@ mongoose.connect(NODE_ENV === 'production' ? MONGO_DB : mongodb, {
   useUnifiedTopology: true,
 });
 
-app.use(cors(corsOptions));
+
+app.route('*')
+  .all(function(req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Origin', 'https://localhost:8080');
+    res.header('Access-Control-Allow-Headers', 'X-API-TOKEN, Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
+
+// app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
