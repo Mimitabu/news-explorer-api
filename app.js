920a825a6fb8723df8c8c4dd2e12b39ddfc8e57d
Marie Slavina <morefly@yandex.ru>
2020-03-08 12:18:44 +0300

    CORS

--- app.js
+++ app.js
@@ -17,42 +17,19 @@ const mongodb = require('./mongodb');
 
 const { NODE_ENV, MONGO_DB } = process.env;
 
-// const whitelist = ['http://localhost:8080', 'http://news-explorer.pw', 'http://api.news-explorer.pw', 'http://localhost:3000'];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// };
-
 mongoose.connect(NODE_ENV === 'production' ? MONGO_DB : mongodb, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
 
-const allowedCors = [
-  'http://news-explorer.pw',
-  'http://news-explorer.pw',
-  'http://www.news-explorer.pw',
-  'http://www.news-explorer.pw',
-  'https://mimitabu.github.io',
-  'https://mimitabu.github.io',
-  'http://localhost:8080',
-];
-
-
-cors({ origin: allowedCors });
-// app.route('*')
-//   .all(function(req, res, next) {
-//     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'X-API-TOKEN, Content-Type, Authorization, Content-Length, X-Requested-With');
-//     next();
-// });
+app.route('*')
+  .all(function(req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-API-TOKEN, Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
